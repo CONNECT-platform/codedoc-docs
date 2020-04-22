988c1bc7a71c5d51ed5c149d0e2b8a95ec9a5eb7
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-04-22 13:41:42 +0200

    add a copyright notice to header

--- .codedoc/content/header.tsx
+++ .codedoc/content/header.tsx
@@ -1,5 +1,5 @@
 import { CodedocConfig } from '@codedoc/core';
-import { Header as _Header, GithubButton, Watermark } from '@codedoc/core/components';
+import { Header as _Header, GithubButton } from '@codedoc/core/components';
 
 
 export function Header(config: CodedocConfig, renderer: any) {
@@ -17,7 +17,35 @@ export function Header(config: CodedocConfig, renderer: any) {
         <br/><br/>
       </fragment>
       : ''}
-      {/* <Watermark/> */}
+      <style>{`
+        .header-copyright-notice {
+          font-size: 10px;
+          display: block;
+          opacity: .5;
+          transition: opacity .3s !important;
+          text-decoration: none !important;
+        }
+
+        .header-copyright-notice img {
+          width: 32px;
+        }
+
+        .header-copyright-notice:hover {
+          opacity: 1;
+        }
+
+        @media screen and (max-width: 800px) {
+          .header-copyright-notice {
+            display: none;
+          }
+        }
+      `}</style>
+      <a href="https://connect-platform.com" target="_blank" class="header-copyright-notice">
+        MIT License<br/>
+        Copyright © 2020<br/>
+        CONNECT-platform<br/>
+        <img src="https://avatars3.githubusercontent.com/u/42326118?s=200&v=4"/>
+      </a>
     </_Header>
   )
 }
